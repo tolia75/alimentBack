22c64a17e74302fb4589d4827ead894006a9df53
tolia75 <aurelienenki@free.fr>
2021-01-05 11:29:30 +0100

    Ajout de l'annotation permettant d'éviter boucle infini

--- src/main/java/com/example/demo/model/Aliment.java
+++ src/main/java/com/example/demo/model/Aliment.java
@@ -1,21 +1,25 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+import java.util.Objects;
 
-@ToString
 @Getter
 @Setter
 @NoArgsConstructor
 
 @Entity
 @Table(name = "al_aliment")
-public class Aliment {
+// Annotation qui évite les boucles infinies lors de récupération de d'entités en BDD
+@JsonIdentityInfo(generator= ObjectIdGenerators.IntSequenceGenerator.class)
+public class Aliment implements Serializable {
     @Id
     @Column(name = "id_aliment")
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
@@ -25,13 +29,38 @@ public class Aliment {
     @Column(name = "type_aliment")
     //@NotEmpty(message = "Le type d'aliment doit être présent")
     private String typeAliment;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "panier_fk")
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     private Panier panier;
 
     public Aliment(Long id, String nom, String typeAliment) {
         this.id = id;
         this.nom = nom;
         this.typeAliment = typeAliment;
+    }
+
+    @Override
+    public String toString() {
+        return "Aliment{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", typeAliment='" + typeAliment + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Aliment aliment = (Aliment) o;
+        return Objects.equals(id, aliment.id) &&
+                Objects.equals(nom, aliment.nom) &&
+                Objects.equals(typeAliment, aliment.typeAliment);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nom, typeAliment);
     }
 }
--- src/main/java/com/example/demo/model/Panier.java
+++ src/main/java/com/example/demo/model/Panier.java
@@ -1,10 +1,15 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.Set;
 
 @NoArgsConstructor
 @Getter
@@ -12,7 +17,9 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "pa_panier")
-public class Panier {
+// Annotation qui évite les boucles infinies lors de récupération de d'entités en BDD
+@JsonIdentityInfo(generator= ObjectIdGenerators.IntSequenceGenerator.class)
+public class Panier implements Serializable {
 
     @Id
     @Column
@@ -27,4 +34,33 @@ public class Panier {
 
     @Column(name = "is_cadeau")
     private boolean isCadeau;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "panier")
+    private Set<Aliment> aliments;
+
+    @Override
+    public String toString() {
+        return "Panier{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prix=" + prix +
+                ", isCadeau=" + isCadeau +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Panier panier = (Panier) o;
+        return prix == panier.prix &&
+                isCadeau == panier.isCadeau &&
+                Objects.equals(id, panier.id) &&
+                Objects.equals(nom, panier.nom);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nom, prix, isCadeau);
+    }
 }
